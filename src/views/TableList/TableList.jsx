d95991c370483fab6eb06f6f16ef63cfb53e455d
Ali <slyroncloudwalker@gmail.com>
2018-11-28 21:43:09 +0200

    Test

---
+++ src/views/TableList/TableList.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+// @material-ui/core components
+import withStyles from "@material-ui/core/styles/withStyles";
+// core components
+import GridItem from "components/Grid/GridItem.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import Table from "components/Table/Table.jsx";
+import Card from "components/Card/Card.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+
+const styles = {
+  cardCategoryWhite: {
+    "&,& a,& a:hover,& a:focus": {
+      color: "rgba(255,255,255,.62)",
+      margin: "0",
+      fontSize: "14px",
+      marginTop: "0",
+      marginBottom: "0"
+    },
+    "& a,& a:hover,& a:focus": {
+      color: "#FFFFFF"
+    }
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    "& small": {
+      color: "#777",
+      fontSize: "65%",
+      fontWeight: "400",
+      lineHeight: "1"
+    }
+  }
+};
+
+function TableList(props) {
+  const { classes } = props;
+  return (
+    <GridContainer>
+      <GridItem xs={12} sm={12} md={12}>
+        <Card>
+          <CardHeader color="primary">
+            <h4 className={classes.cardTitleWhite}>Fleet</h4>
+            <p className={classes.cardCategoryWhite}>
+              Active drivers for a given set of routes
+            </p>
+          </CardHeader>
+          <CardBody>
+            <Table
+              tableHeaderColor="primary"
+              tableHead={["Name", "Start Location", "Current Location", "Occupancy"]}
+              tableData={[
+                ["Driver", "Mohandesseen", "Tahrir", "36"],
+                ["Driver", "Tahrir", "Ring Road", "23"],
+                ["Driver", "October", "Ring Road", "26"],
+                ["Driver", "Mohandesseen", "Mehwar", "38"],
+                ["Driver", "Maadi", "Faisal", "43"],
+                ["Driver", "Nasr City", "October", "48"]
+              ]}
+            />
+          </CardBody>
+        </Card>
+      </GridItem>
+    </GridContainer>
+  );
+}
+
+export default withStyles(styles)(TableList);
